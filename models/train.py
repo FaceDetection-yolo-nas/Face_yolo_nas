2834a326151d44161aaabb57fbff6b30cebc560e
TatianeIvanof <65306179+TatianeIvanof@users.noreply.github.com>
2023-12-02 02:11:08 -0300

    Create train.py
     Treinamento do modelo

---
+++ models/train.py
@@ -0,0 +1,50 @@
+train_params = {
+    'silent_mode': False,
+    "average_best_models":True,
+    "warmup_mode": "LinearBatchLRWarmup",
+    "warmup_initial_lr": 1e-6,
+    "lr_warmup_epochs": 3,
+    "initial_lr": 5e-4, # aumentei
+    "lr_mode": "CosineLRScheduler",
+    "cosine_final_lr_ratio": 0.1,
+    "optimizer": "AdamW",
+    "optimizer_params": {"weight_decay": 0.0001},
+    "zero_weight_decay_on_bias_and_bn": True,
+    "ema": True,
+    "ema_params": {"decay": 0.9999, "decay_type": "exp", "beta": 15}, # Ajustei parâmetros do EMA para EXP
+    "max_epochs": MAX_EPOCHS,
+    "mixed_precision": True,
+    "loss": PPYoloELoss(
+        use_static_assigner=False,
+        num_classes=len(dataset_params['classes']),
+        reg_max=16
+        ),
+    "valid_metrics_list": [
+        DetectionMetrics_050(
+            score_thres=0.1,
+            top_k_predictions=300,
+            num_cls=len(dataset_params['classes']),
+            normalize_targets=True,
+            post_prediction_callback=PPYoloEPostPredictionCallback(
+                score_threshold=0.01,
+                nms_top_k=1000,
+                max_predictions=300,
+                nms_threshold=0.5, # # Reduzi a threshold do NMS para uma detecção mais suave e aumentar a detecção de objetos menores
+
+
+            )
+        )
+    ],
+    "metric_to_watch": 'mAP@0.50'
+}
+
+trainer = Trainer(experiment_name=EXPERIMENT_NAME, ckpt_root_dir=CHECKPOINT_DIR)
+
+
+try:
+    trainer.train(model=model,
+              training_params=train_params,
+              train_loader=train_data,
+              valid_loader=val_data)
+except Exception as e:
+    print(f"Ocorreu um erro durante o treinamento: {e}")
