40a7d3a4343f3f650652ab872b7233a42eb7401b
TatianeIvanof <65306179+TatianeIvanof@users.noreply.github.com>
2023-12-04 19:32:08 -0300

    Update transformacao.py
    Arquivo com transformações de imagem

--- utils/transformacao.py
+++ utils/transformacao.py
@@ -1,1 +1,22 @@
+train_params = 
 
+{
+    'silent_mode': False,
+    "average_best_models":True,
+    "warmup_mode": "LinearBatchLRWarmup",
+    "warmup_initial_lr": 1e-6,
+    "lr_warmup_epochs": 3,
+    "initial_lr": 5e-4,
+    "lr_mode": "CosineLRScheduler",
+    "cosine_final_lr_ratio": 0.1,
+    "optimizer": "AdamW",
+    "optimizer_params": {"weight_decay": 0.0001},
+    "zero_weight_decay_on_bias_and_bn": True,
+    "ema": True,
+    "ema_params": {"decay": 0.9999, "decay_type": "exp", "beta": 15},
+    "max_epochs": MAX_EPOCHS,
+    "mixed_precision": True, ...
+...
+
+
+}
