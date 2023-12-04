4eb07aa2d31b2a763757b868fb284fd3c198978c
TatianeIvanof <65306179+TatianeIvanof@users.noreply.github.com>
2023-12-04 19:27:32 -0300

    Update metrics.py
    Arquivo com transformações de imagem

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,1 +1,25 @@
+train_params = {
 
+  "loss": PPYoloELoss(
+        use_static_assigner=False,
+        num_classes=len(dataset_params['classes']),
+        reg_max=16
+        ),
+    "valid_metrics_list": [
+        DetectionMetrics_050(
+            score_thres=0.1,
+            top_k_predictions=300,
+            num_cls=len(dataset_params['classes']),
+            normalize_targets=True,
+            post_prediction_callback=PPYoloEPostPredictionCallback(
+                score_threshold=0.01,
+                nms_top_k=1000,
+                max_predictions=300,
+                nms_threshold=0.5,
+
+
+            )
+        )
+    ],
+    "metric_to_watch": 'mAP@0.50'
+}
