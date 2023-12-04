adbcb6f539ed55ecaa2f18bdf9d0cbb0e0b10598
TatianeIvanof <65306179+TatianeIvanof@users.noreply.github.com>
2023-12-04 19:34:51 -0300

    Update visualization.py
    Arquivo para visualização de resultados

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,1 +1,29 @@
+from super_gradients.training import Trainer
+
+trainer = Trainer(experiment_name=EXPERIMENT_NAME, ckpt_root_dir=CHECKPOINT_DIR)
+     
+
+trainer.train(model=model,
+              training_params=train_params,
+              train_loader=train_data,
+              valid_loader=val_data)
+     
+
+
+best_model = models.get(
+    MODEL_ARCH,
+    num_classes=len(dataset_params['classes']),
+    checkpoint_path=f"average_model.pth"
+).to(DEVICE)
+
+%load_ext tensorboard
+%tensorboard --logdir {CHECKPOINT_DIR}/{EXPERIMENT_NAME}
+   
+img_1 = ""
+best_model.predict(img_1).show()
+     
+
+img_2 = ""
+best_model.predict(img_2).show()
+
 
