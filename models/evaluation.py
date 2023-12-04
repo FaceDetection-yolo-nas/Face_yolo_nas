887166323dfaed71c96a83bf5140a325212e7bb7
TatianeIvanof <65306179+TatianeIvanof@users.noreply.github.com>
2023-12-04 19:36:30 -0300

    Update evaluation.py
    atualização

--- models/evaluation.py
+++ models/evaluation.py
@@ -1,5 +1,5 @@
 best_model = models.get(
     MODEL_ARCH,
     num_classes=len(dataset_params['classes']),
-    checkpoint_path=checkpoint_file
+    checkpoint_path=f"average_model.pth"
 ).to(DEVICE)
