674dcb0be9cd300550eaea812a136c0926b842ec
Lucas Aragao Alves da Costa <30377854+lucasarags@users.noreply.github.com>
2023-12-04 23:42:45 -0300

    Update training.py

--- utils/training.py
+++ utils/training.py
@@ -1,3 +1,4 @@
+'''
 from super_gradients.training import Trainer
 
 trainer = Trainer(experiment_name=EXPERIMENT_NAME, ckpt_root_dir=CHECKPOINT_DIR)
@@ -6,3 +7,4 @@ trainer.train(model=model,
               training_params=train_params,
               train_loader=train_data,
               valid_loader=val_data)
+'''
