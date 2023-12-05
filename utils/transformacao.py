b1a462ac2483db8f550d921668a96005521894d2
Lucas Aragao Alves da Costa <30377854+lucasarags@users.noreply.github.com>
2023-12-04 22:16:58 -0300

    Update transformacao.py
    putting the complete coding

--- utils/transformacao.py
+++ utils/transformacao.py
@@ -1,6 +1,4 @@
-train_params = 
-
-{
+train_params = {
     'silent_mode': False,
     "average_best_models":True,
     "warmup_mode": "LinearBatchLRWarmup",
@@ -15,8 +13,26 @@ train_params =
     "ema": True,
     "ema_params": {"decay": 0.9999, "decay_type": "exp", "beta": 15},
     "max_epochs": MAX_EPOCHS,
-    "mixed_precision": True, ...
-...
-
-
+    "mixed_precision": True, 
+    "loss": PPYoloELoss(
+        use_static_assigner=False,
+        num_classes=len(dataset_params['classes']),
+        reg_max=16
+    ),
+    "valid_metrics_list": [
+        DetectionMetrics_050(
+            score_thres=0.1,
+            top_k_predictions=300,
+            num_cls=len(dataset_params['classes']),
+            normalize_targets=True,
+            post_prediction_callback=PPYoloEPostPredictionCallback(
+                score_threshold=0.01,
+                nms_top_k=1000,
+                max_predictions=300,
+                nms_threshold=0.7
+            )
+        )
+    ],
+    "metric_to_watch": 'mAP@0.50'
+}
 }
